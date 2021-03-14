5d0e8e87fde911887d238cbbc7935ca5f4727573
raeruiqizhang <rzhang1@babson.edu>
2021-03-14 13:57:26 -0400

    update

--- groupwork.py
+++ groupwork.py
@@ -1,6 +1,12 @@
 import urllib.request
 
-url = 'http://www.gutenberg.org/files/1342/1342-0.txt' #Pride and Prejudice
+url = 'http://www.gutenberg.org/files/1342/1342-0.txt' # Pride and Prejudice
+url = 'http://www.gutenberg.org/files/158/158-0.txt' # Emma
+url = 'http://www.gutenberg.org/files/161/161-0.txt' # Sense and Sensibility
+url = 'http://www.gutenberg.org/ebooks/105.txt.utf-8'# Persuasion
+url = 'http://www.gutenberg.org/files/121/121-0.txt' # Northanger Abbey
+url = 'http://www.gutenberg.org/files/141/141-0.txt' # Mansfield Park
+
 response = urllib.request.urlopen(url)
 data = response.read()  # a `bytes` object
 text = data.decode('utf-8')
