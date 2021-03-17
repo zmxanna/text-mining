9ae50288f8d28c2a9f1092a739afa2e7c798a72b
zmxanna <zmxanna@gmail.com>
2021-03-16 23:50:52 -0400

    Update possible_functions.py

--- possible_functions.py
+++ possible_functions.py
@@ -1,3 +1,4 @@
+# function 1:
 def skip_gutenberg_header(fp):
     """Reads from fp until it finds the line that ends the header.
     fp: open file object
@@ -7,14 +8,18 @@ def skip_gutenberg_header(fp):
             break
     #    can also do 'end with the project' in process_file
 
+# function 2:
 def total_words(hist):
     """Returns the total of the frequencies in a histogram."""
     return sum(hist.values())
 
+# function 3:
 def different_words(hist):
     """Returns the number of different words in a histogram."""
     return len(hist)
 
+
+# function 4:
 def most_common(hist, excluding_stopwords=False):
     """Makes a list of word-freq pairs in descending order of frequency.
 
@@ -38,6 +43,8 @@ def most_common(hist, excluding_stopwords=False):
     common_words.sort(reverse=True)
     return common_words
 
+
+# function 5:
 def print_most_common(hist, num=10):
     """Prints the most commons words in a histgram and their frequencies.
     hist: histogram (map from word to frequency)
@@ -47,5 +54,5 @@ def print_most_common(hist, num=10):
     for freq, word in hist[0:num]:
         print(word, '\t', freq)
 
-
+# function 6:
 common words between file
